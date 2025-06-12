c0afcaa4906b71c925dcf25b99808012df8760fa
mackemo <mackenzielmoore14@gmail.com>
2025-06-12 17:36:13 -0500

    Added game statements

---
+++ game.js
@@ -0,0 +1,25 @@
+const emotionStatements = [
+    { text: "Happy", type: "good" },
+    { text: "Confident", type: "good" },
+    { text: "Calm", type: "good" },
+    { text: "Relaxed", type: "good" },
+    { text: "Angry", type: "bad" },
+    { text: "Sad", type: "bad" },
+    { text: "Scared", type: "bad" },
+    { text: "Surprised", type: "bad" }
+];
+
+const ruleStatements = [
+    { text: "Listen to your coach and follow instructions", type: "good" },
+    { text: "No running or loud sounds at the barn", type: "good" },
+    { text: "Never walk or stand directly behind or in front of the horse", type: "good" },
+    { text: "Praise often. Correct seldom. Never punish", type: "good" },
+    { text: "Don't wrap or tie anything around your hand or body", type: "good" },
+    { text: "Only feed treats in a bucket", type: "good" },
+    { text: "Use your hands to spank your horse if he misbehaves", type: "bad" },
+    { text: "Tie the lead rope to your wrist so you don't lose your horse", type: "bad" },
+    { text: "Shout hello to your coach when you get to the barn", type: "bad" },
+    { text: "If your horse is facing away from you, run up behind them and turn them around", type: "bad" },
+    { text: "You should run to get your saddle so you aren't late", type: "bad" },
+    { text: "Tell your horse he's stupid when he makes a mistake", type: "bad" }
+];
